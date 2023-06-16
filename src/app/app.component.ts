987659f790030e5a9e0e3aea19af736ca4921430
Joaquim Pujol <joaquim.pujol@gmail.com>
2023-06-16 21:10:30 +0200

    Angular-Simple-CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  employeeArr: Employee[] = [
+    {id: 1, name: 'Albert', country: 'Spain'},
+    {id: 2, name: 'Eve', country: 'USA'},
+    {id: 3, name: 'Jordan', country: 'United Kingdom'},
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArr.length +1;
+      this.employeeArr.push(this.selectedEmployee);
+    }
+
+    this.selectedEmployee = new Employee();
+  }
+
+  openForEdit(employee: Employee) { 
+    this.selectedEmployee = employee;
+  }
+
+  delete() {
+    if(confirm('Are you sure you want delete it?')){
+      this.employeeArr = this.employeeArr.filter(e => e != this.selectedEmployee); // Por cada elemento de array que sea diferente al elemento seleccionado
+      this.selectedEmployee = new Employee();
+    }
+  }
 }
